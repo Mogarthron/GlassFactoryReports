df84214e1e3177da093abba6df171b0d07884a38
Wit Łuczyński <witluczynski@gmail.com>
2020-04-08 14:56:56 +0200

    8.4.2020
    Add some controlls

--- BachPlantDesktop/MainWindow.xaml.cs
+++ BachPlantDesktop/MainWindow.xaml.cs
@@ -20,11 +20,36 @@ namespace BachPlantDesktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] LanguageVer = { "Polski", "Englisch" };
+        string[] AppTitle = { "Zestawiarnia", "Bach Plant" };
+        string[] RecipesTabControllItem = { "Receptury", "Recipes" };
+        string[] BachesTabControllItem = { "Zestawy", "Baches" };
+
         public MainWindow()
         {
             InitializeComponent();
-            TCIRecipes.Header = "Receptury";
-            TCIBaches.Header = "Zesatwy";
+            Title = AppTitle[0];
+            TCIRecipes.Header = RecipesTabControllItem[0];
+            TCIBaches.Header = BachesTabControllItem[0];
+            BtnAddRecipe.Content = "Dodaj Recepturę";
+            MLEnglisch.Header = LanguageVer[1];
+            MLPolisch.Header = LanguageVer[0];
+        }
+
+        private void MLPolisch_Click(object sender, RoutedEventArgs e)
+        {
+            Title = AppTitle[0];
+            TCIRecipes.Header = RecipesTabControllItem[0];
+            TCIBaches.Header = BachesTabControllItem[0];
+            BtnAddRecipe.Content = "Dodaj Recepturę";          
+        }
+
+        private void MLEnglisch_Click(object sender, RoutedEventArgs e)
+        {
+            Title = AppTitle[1];
+            TCIRecipes.Header = RecipesTabControllItem[1];
+            TCIBaches.Header = BachesTabControllItem[1];
+            BtnAddRecipe.Content = "Add Recipe";
         }
     }
 }
